fa5b8f95af90a883976235007f0b7fa31811a4d2
RSwooflov <rianasantos808@gmail.com>
2017-02-23 16:20:43 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function onClick(){
+	document.getElementById("asdf").innerHTML = myDiv;
+};	
+
+document.getElementById("myDiv").style.backgroundColor = "green"
+
+function changeBackground(color) {
+	document.body.style.background =color;
+}
+function link (){
+	window.location.href="http://www.hba.net"
+	//document.getElementById("myDiv").innerHTML
+}
